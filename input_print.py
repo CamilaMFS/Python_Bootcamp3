9add73899f537c37c6eb1e725986cd85e58f16ba
Camila Marques <mfs.cami@gmail.com>
2024-06-18 21:22:00 -0300

    Add files via upload

---
+++ input_print.py
@@ -0,0 +1,7 @@
+nome = input("Informe seu nome: ")
+idade = input("Informe sua idade: ")
+
+print(nome,idade)
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#", end="...\n")
+print(nome, idade, sep="#")
